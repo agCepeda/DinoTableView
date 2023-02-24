cf71b64f163b100c8b28c1e8d2d32ee7c2b3106f
agCepeda <aj.cepeda.hdz@gmail.com>
2023-02-24 12:57:47 -0600

    Initial Commit

---
+++ DinoTableView/DinoTableView.h
@@ -0,0 +1,18 @@
+//
+//  DinoTableView.h
+//  DinoTableView
+//
+//  Created by Agustin Cepeda on 24/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DinoTableView.
+FOUNDATION_EXPORT double DinoTableViewVersionNumber;
+
+//! Project version string for DinoTableView.
+FOUNDATION_EXPORT const unsigned char DinoTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DinoTableView/PublicHeader.h>
+
+
